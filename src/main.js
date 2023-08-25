bc590f2b16431cf7ab59fbe1594d0f4b2c1dcac0
Lauro Villarreal <lauro.villarreal@r2devpros.com>
2023-08-24 18:07:15 -0600

    validaciones para crear una nueva venta

--- src/main.js
+++ src/main.js
@@ -5,11 +5,18 @@ import { createPinia } from 'pinia'
 import { authentication } from './plugins/authentication'
 
 
+/* Se agrega dependencia de Vue-picker */
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+
 /* Aqui se pueden poner los estilos para el portal (personales, templeate, material) */ 
  import 'bootstrap/dist/css/bootstrap.css'
  import "bootstrap"
 
 const app = createApp(App);
+
+app.component('VueDatePicker', VueDatePicker);
 
 app.use(createPinia())
 
--- src/views/ventas/VentaNuevoView.vue
+++ src/views/ventas/VentaNuevoView.vue
@@ -6,19 +6,29 @@
                     <h1>Nueva venta</h1>
                     <p v-if="errorMessage" class="error-message text-danger mb-4">{{errorMessage}}</p>
 
+                    <h3>Operador</h3>
                     <div class="mb-3 col-8">
-                        <label for="venta" class="form-label">venta</label>
-                        <input v-model="ventaDet.nombre" type="text" class="form-control" id="venta" placeholder="Nombre del venta">
+                        <label for="operador" class="form-label">Operador</label>
+                
+                        <select v-model="ventaDet.operador.operadorId" class="form-select" aria-label="Default select example">
+                            <option :value="0" selected>Selecciona un proveedor</option>
+                            <option v-for="operador in operadores" :key="operador._id" :value="operador._id">
+                                {{ operador.nombre }}
+                            </option>
+                        </select>
                     </div>
 
                     <div class="mb-3 col-3">
-                        <label for="siglas" class="form-label">Siglas</label>
-                        <input v-model="ventaDet.siglas" type="text" class="form-control" id="siglas" placeholder="Siglas del venta">
+                        <label for="claveReserva" class="form-label">Clave de Reserva</label>
+                        <input v-model="ventaDet.telefono" type="text" class="form-control" id="claveReserva" placeholder="Clave de la Reserva">
                     </div>
 
+                    <hr/>
+                     <h3>Datos de la Reserva</h3>
+
                     <div class="mb-3 col-8">
-                        <label for="nombreContacto" class="form-label">Nombre Contacto</label>
-                        <input v-model="ventaDet.nombreContacto" type="text" class="form-control" id="nombreContacto" placeholder="Nombre del contacto del venta">
+                        <label for="titular" class="form-label">Titular</label>
+                        <input v-model="ventaDet.titular" type="text" class="form-control" id="titular" placeholder="Nombre del titular">
                     </div>
 
                     <div class="mb-3 col-3">
@@ -26,9 +36,132 @@
                         <input v-model="ventaDet.telefono" type="tel" class="form-control" id="telefono" placeholder="Telefono">
                     </div>
 
+                    <div class="mb-3 col-6">
+                        <label for="telefono" class="form-label">Email</label>
+                        <input v-model="ventaDet.telefono" type="tel" class="form-control" id="telefono" placeholder="Email">
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Fecha Limite</label>
+                        <input v-model="ventaDet.telefono" type="tel" class="form-control" id="telefono" placeholder="Fecha Limite">
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Total Venta</label>
+                        <div class="input-group">
+                            <span class="input-group-text">$</span>
+                            <input v-model="ventaDet.totalVenta" 
+                            @input="event => ventaDet.notaVenta.resto = event.target.value"
+                            type="text" class="form-control" id="telefono" placeholder="Total Venta">
+                        </div>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Total Neto</label>
+                        <div class="input-group">
+                            <span class="input-group-text">$</span>
+                            <input v-model="ventaDet.totalNeto" type="text" class="form-control" id="telefono" placeholder="Total Venta">
+                        </div>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                    </div>
+
+                    <div class="mb-3 col-4">
+                         <input class="form-check-input" type="checkbox" v-model="ventaDet.ventaDls" id="chkVentadls">
+                         <label for="chkVentadls" class="form-label">Venta Dls</label>
+                    </div>
+
                     <div class="mb-3 col-8">
-                        <label for="email" class="form-label">Email</label>
-                        <input v-model="ventaDet.emailContacto" type="email" class="form-control" id="email" placeholder="Email">
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Fecha Viaje</label>
+                        <VueDatePicker v-model="ventaDet.detalleViaje.fechaViaje" :enable-time-picker="false" />
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Hora Viaje</label>
+                        <VueDatePicker v-model="ventaDet.detalleViaje.horaViaje" time-picker :is-24="false"/>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Fecha Regreso</label>
+                        <VueDatePicker v-model="ventaDet.detalleViaje.fechaRegreso" :enable-time-picker="false" />
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Hora Regreso</label>
+                        <VueDatePicker v-model="ventaDet.detalleViaje.horaRegreso" time-picker :is-24="false"/>
+                    </div>
+
+                    <div class="mb-3 col-6">
+                        <label for="titular" class="form-label">Hotel</label>
+                        <input v-model="ventaDet.titular" type="text" class="form-control" id="titular" placeholder="Hotel">
+                    </div>
+
+                    <div class="mb-3 col-6">
+                    </div>
+
+                    <div class="mb-3 col-10">
+                        <label for="titular" class="form-label">Descripcion</label>
+                        <textarea rows="4" v-model="ventaDet.titular" type="text" class="form-control" id="titular" placeholder="Descripcion"></textarea>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                         <input class="form-check-input" type="checkbox" v-model="ventaDet.detalleViaje.vuelosRedondos" id="flexCheckDefault">
+                         <label for="flexCheckDefault" class="form-label">Vuelos Redondos</label>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                         <input class="form-check-input" type="checkbox" v-model="ventaDet.detalleViaje.vuelosRedondos" id="flexCheckDefault">
+                         <label for="flexCheckDefault" class="form-label">Todo Incluido</label>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                         <input class="form-check-input" type="checkbox" v-model="ventaDet.detalleViaje.vuelosRedondos" id="flexCheckDefault">
+                         <label for="flexCheckDefault" class="form-label">Traslados</label>
+                    </div>
+
+                    <hr/>
+                     <h3>Pagos</h3>
+
+                    <div class="mb-3 col-2">
+                        <label for="nombreContacto" class="form-label">Folio</label>
+                        <input v-model="ventaDet.nombreContacto" type="text" class="form-control" id="nombreContacto" placeholder="Folio de la venta">
+                    </div>
+
+                    <div class="mb-3 col-10">
+                        <label for="titular" class="form-label">Concepto</label>
+                        <textarea rows="4" v-model="ventaDet.titular" type="text" class="form-control" id="titular" placeholder="Descripcion"></textarea>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Importe</label>
+                        <input v-model="ventaDet.notaVenta.importe"
+                            @input="event => ventaDet.notaVenta.importeMXN = event.target.value * ventaDet.notaVenta.tipoCambio" 
+                            type="text" class="form-control" id="telefono" placeholder="Importe">
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Tipo Cambio</label>
+                        <input v-model="ventaDet.notaVenta.tipoCambio" type="tel" class="form-control" id="telefono" placeholder="Tipo Cambio" :disabled="!ventaDet.ventaDls">
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Importe MXN</label>
+                        <input v-model="ventaDet.notaVenta.importeMXN" type="tel" class="form-control" id="telefono" placeholder="Importe MXN" disabled>
+                    </div>
+
+                    <div class="mb-3 col-4">
+                        <label for="telefono" class="form-label">Resto</label>
+                        <input v-model="ventaDet.notaVenta.resto" type="tel" class="form-control" id="telefono" placeholder="Resto de la reserva" disabled>
+                    </div>
+
+                    <div class="mb-3 col-6">
                     </div>
 
                     <div class="mb-3 mt-4 col-3 d-sm-flex">
@@ -46,15 +179,59 @@
 
 /* eslint-disable */
 
-    import { reactive, ref } from "vue";
+    import { useOperadorStore } from "../../stores/operador";
+    import { reactive, ref, computed, onMounted } from "vue";
 
     const ventaDet = reactive({
+        operador: {
+            operadorId:"0",
+            nombre: ""
+        },
+        detalleViaje:{
+            vuelosRedondos: false,
+            fechaViaje: new Date(),
+            horaViaje: {
+                hours: new Date().getHours(),
+                minutes: new Date().getMinutes()
+            },
+            fechaRegreso:new Date(),
+            horaRegreso: {
+                hours: new Date().getHours(),
+                minutes: new Date().getMinutes()
+            }
+        },
+        totalVenta: 0,
+        totalNeto: 0,
+        ventaDls: false,
         nombre: "",
         siglas: "",
         nombreContacto: "",
         telefono: "",
-        emailContacto: ""
+        emailContacto: "",
+        notaVenta:{
+            folio:"",
+            concepto: "",
+            importe: 0,
+            tipoCambio: 1,
+            importeMXN: 0,
+            resto: 0
+        }
     })
+
+
+    const operadorAuth = useOperadorStore();
+
+    const operadores = computed(() => {
+    return operadorAuth.listaOperadores;
+    });
+
+    async function getOperadores() {
+    await operadorAuth.obtenerOperadores();
+    }
+
+    onMounted(async () => {
+        await getOperadores();
+    });
 
 </script>
 
